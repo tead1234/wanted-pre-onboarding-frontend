fd2d7889a0f972c78b05cec27c3b22be59d20791
tead1234 <tead1234@gmail.com>
2023-04-05 22:24:07 +0900

    이것저것

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import CreateAccount from './Pages/CreateAccount';
 import Login from './Pages/Login';
+import Todo from './Pages/Todo'
+
 function App() {
   // routers
 
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage : "url(/bg1.jpg)"}}>
         <BrowserRouter>
 				<Routes>
 					<Route path="/signup" element={<CreateAccount></CreateAccount>}></Route>
 					<Route path="/signin" element={<Login></Login>}></Route>
+					<Route path="/todo" element={<Todo></Todo>}></Route>
 					<Route path="*" element={<></>}></Route>
 				</Routes>
 			</BrowserRouter>
---
+++ src/Pages/Todo.js
@@ -0,0 +1,39 @@
+export default function Todo() {
+    // 약간 필기체 느낌으로 적는거면 좋겠음
+    // 전체 할일을 관리하는 state를 만들고 state의 갯수만큼 반복문으로 생성하면 될듯??
+    // checkbox를 누르면 취소선 생기는 효과넣기
+    return(
+        <>
+            <div>
+                <h2>오늘 할일</h2>
+            </div>
+            <div className="flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
+                <div>
+                <label htmlFor="todo">밥먹기</label>
+                <input
+                    type="checkbox"
+                    name="todo"
+                >
+                </input>
+                </div>
+                <div>
+                <label htmlFor="todo">밥먹기</label>
+                <input
+                    type="checkbox"
+                    name="todo"
+                >
+                </input>
+                </div>
+                <div>
+                <label htmlFor="todo">밥먹기</label>
+                <input
+                    type="checkbox"
+                    name="todo"
+                >
+                </input>
+                </div>
+                
+            </div>
+        </>
+    );   
+}
