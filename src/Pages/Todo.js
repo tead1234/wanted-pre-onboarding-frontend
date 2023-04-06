32a589f77b326cf63867c08a03a3248395abdae7
tead1234 <tead1234@gmail.com>
2023-04-06 22:23:09 +0900

    ㅁㄹ

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -2,6 +2,8 @@ export default function Todo() {
     // 약간 필기체 느낌으로 적는거면 좋겠음
     // 전체 할일을 관리하는 state를 만들고 state의 갯수만큼 반복문으로 생성하면 될듯??
     // checkbox를 누르면 취소선 생기는 효과넣기
+
+    /// state 관리
     return(
         <>
             <div>
