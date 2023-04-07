c2efe1755cd8fbaa7cde32683846252fc651d894
tead1234 <tead1234@gmail.com>
2023-04-07 16:19:52 +0900

    삭제버튼, 취소선 기능 추가

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -11,19 +11,45 @@ export default function Todo() {
     e.preventDefault();
     const newTodo = {id : id, value : inputRef.current.value};
     setId(id += 1);
-    console.log(id);
     console.log(todoList);
     setTodoList([...todoList, newTodo]);
     inputRef.current.value = "";
   };
 
-  const checkbox_clicked = (e) => {
+  const checkbox_clicked = (id) => {
     // TODO: 체크박스 클릭 이벤트 핸들러 함수 구현
     // 바로 체크박스로 구현하는건 힘들고 별도의 state를 활용해서 개발해야될거같음
     // checkbox를 클릭할때마다 index값을 complete에 저장하고 
     // 저장된 인덱스들은 취소선 css 값 주기
-    alert(e.target.value);
+    // state에서 id값을 가진애를 삭제
+    // undone도 가능하도록 구현
+    if(checkComplete.includes(id)){
+        let renew = checkComplete.filter(ch => ch !== id);
+        setCheckComplete(renew);
+    }else{
+        setCheckComplete([...checkComplete, id]);
+    }
+
+    
   };
+  // 삭제 버튼을 누르면 해당 li의 아이디를 state에서 지우면 됨
+const check = (id) => {
+    let renew = [];
+    todoList.map((todo) => {
+        if(todo.id !== id){
+            renew.push(todo);
+        }
+    });
+    setTodoList(renew);
+}
+// 수정 버튼
+const modifyCheck = (id) =>{
+    // 수정창을 띄우고
+    // 내용을 입력후 확인 버튼을 누르면 
+    // checkC todolist를 수정하면 ok 
+    // checkbox 클릭 유무는 어차피 id로 관리 되고 있어서 상관없음
+    
+}
 
   return (
     <div>
@@ -43,10 +69,17 @@ export default function Todo() {
             <input
               type="checkbox"
               name={`todo-item-${todo.id}`}
-              onClick={checkbox_clicked}
+              onClick={() => checkbox_clicked(todo.id)}
               
             />
-            <label htmlFor={`todo-item-${todo.id}`}>{todo.value}</label>
+            {
+                checkComplete.includes(todo.id)? 
+                <label style={{textDecoration : "line-through"}} htmlFor={`todo-item-${todo.id}`}>{todo.value}</label> :
+                <label htmlFor={`todo-item-${todo.id}`}>{todo.value}</label>
+
+            }
+            <button onClick={() => check(todo.id)}>삭제</button>
+            <button onClick={() => check(todo.id)}>수정</button>
           </li>
         ))}
       </ul>
