5a138aa31d9c8652bcb6658eaaa92ccd1ec3b495
tead1234 <tead1234@gmail.com>
2023-04-07 15:30:24 +0900

    makev

--- src/Pages/Todo.js
+++ src/Pages/Todo.js
@@ -1,13 +1,18 @@
 import { useState, useRef } from "react";
 
 export default function Todo() {
-  const [todoList, setTodoList] = useState([]);
-
-  const inputRef = useRef(null);
+  let [todoList, setTodoList] = useState([]);
+  let [checkComplete, setCheckComplete] = useState([]);
+  // id 값을 주기 위한 state
+  let [id , setId] = useState(0);
+  let inputRef = useRef(null);
 
   const todo_make = (e) => {
     e.preventDefault();
-    const newTodo = inputRef.current.value;
+    const newTodo = {id : id, value : inputRef.current.value};
+    setId(id += 1);
+    console.log(id);
+    console.log(todoList);
     setTodoList([...todoList, newTodo]);
     inputRef.current.value = "";
   };
@@ -15,6 +20,9 @@ export default function Todo() {
   const checkbox_clicked = (e) => {
     // TODO: 체크박스 클릭 이벤트 핸들러 함수 구현
     // 바로 체크박스로 구현하는건 힘들고 별도의 state를 활용해서 개발해야될거같음
+    // checkbox를 클릭할때마다 index값을 complete에 저장하고 
+    // 저장된 인덱스들은 취소선 css 값 주기
+    alert(e.target.value);
   };
 
   return (
@@ -29,14 +37,16 @@ export default function Todo() {
       <ul>
         {/* 여기서 인덱스를 쓰면 나중에 꼬일 수 있음 */}
         {/* 고유값을 부여해서 만드는게 나을거같음 */}
-        {todoList.map((todo, index) => (
-          <li key={index}>
+        {/* input의 onChange가 과연 언제 발생할까?? */}
+        {todoList.map((todo) => (
+          <li key={todo.id}>
             <input
               type="checkbox"
-              name={`todo-item-${index}`}
+              name={`todo-item-${todo.id}`}
               onClick={checkbox_clicked}
+              
             />
-            <label htmlFor={`todo-item-${index}`}>{todo}</label>
+            <label htmlFor={`todo-item-${todo.id}`}>{todo.value}</label>
           </li>
         ))}
       </ul>
